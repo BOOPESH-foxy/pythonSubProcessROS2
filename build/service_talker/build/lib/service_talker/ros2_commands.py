61dd24606e059c96987a00e53b978d2c385aca7c
Boopesh <boopesh.mc@gmail.com>
2025-04-11 11:09:08 +0530

    initial commit

--- build/service_talker/build/lib/service_talker/ros2_commands.py
+++
@@ -1,2 +0,0 @@
-SourceCommands =["source /opt/ros/humble/setup.zsh ; ros2 run demo_nodes_cpp talker --ros-args --enclave /talker_listener/talker"]
-serviceCallCommand = []
--- build/service_talker/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/roots/PythonRos2_scripting/install/service_talker'
